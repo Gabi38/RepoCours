47a1bacc6411a86eb32906c87a47c2c58544e952
Gabi38 <bastiangabriel@hotmail.com>
2014-04-25 15:57:11 +0200

    Allumage LED Blink M - Mbed :Fonctionnel
    Allume la LED BlinkM en Vert, et l'éteind, après affectation de
    l'adresse et vérification.

--- Mbed_Avec_LCD.c
+++ Mbed_Avec_LCD.c
@@ -1,5 +1,5 @@
 #include "mbed.h"
-#define version "4.0 - Freebox" // affichage Freebox en initialisation sur un SAA1064 avec un MBED NPC1768
+#define version "6.5 - Blink M" // Allume en vert une Diode LED BlinkM avec un MBED NPC1768
 
 Serial pc(USBTX, USBRX);
 I2C i2c(p28, p27); // SDA, SCL 
@@ -10,53 +10,30 @@ int main()
    wait(1);    
    pc.printf("\n Version = %s \n",version); // Affiche la version du programme sur l'ordinateur (lisible via Putty, HyperTerminal  ou TeraTerm)
    wait(1);
-   
+                    //  R      G    B
+   char cmd1[] = {'n', 0x00, 0xff, 0x00 }; // Permet d'eclairer en Vert.
+   char cmd2[] = {'n', 0x00, 0x00, 0x00 }; // permet d'eteindre toute les LEDS.
+     
+   char cmd3[] = {'A', 0x01,0xd0,0x0d,0x01 }; // Ecrit la nouvelle adresse de la LED.  ici : 01
+   i2c.write(0x00,cmd3,5); // Ecrit l'adresse sur la LED.
+   wait(1); // Attend 1 seconde.
+
+   char adresse[] = {'a'}; // Lit l'adresse
+   int add =  i2c.read(0x00,adresse,1); // Lit l'adresse de la led sur toute les adresses (broadcast sur 0x00)
+   pc.printf("\n ADRESSE = %d ",add); // retourne a l'ecran la nouvelle adresse.
+   wait(1);
+
+   char cmd4[] = {'o'};  // Arrete le script de Demo.
+   i2c.write(0x01,cmd4,1);// Arrete le script de Demo.
+   wait(0.1);
 
    while(1) 
-   {
-        // boucle d'affichage lors de l'initialisation de la Freebox.
-        char cmd1[] = {0x00,0x37,0x01,0x0,0x0,0x0}; 
-        char cmd2[] = {0x00,0x37,0x0,0x01,0x0,0x0};
-        char cmd3[] = {0x00,0x37,0x0,0x0,0x01,0x0}; 
-        char cmd4[] = {0x00,0x37,0x0,0x0,0x0,0x01}; 
-        char cmd5[] = {0x00,0x37,0x0,0x0,0x0,0x02};
-        char cmd6[] = {0x00,0x37,0x0,0x0,0x0,0x04};
-        char cmd7[] = {0x00,0x37,0x0,0x0,0x0,0x08};
-        char cmd8[] = {0x00,0x37,0x0,0x0,0x08,0x0};
-        char cmd9[] = {0x00,0x37,0x0,0x08,0x0,0x0};
-        char cmd10[] = {0x00,0x37,0x08,0x0,0x0,0x0};
-        char cmd11[] = {0x00,0x37,0x10,0x0,0x0,0x0};
-        char cmd12[] = {0x00,0x37,0x20,0x0,0x0,0x0};
+   {          
+        //  Affiche lumiere Verte sur une BlinkM.
+        i2c.write(0x01,cmd1,4); // Allume la LED en vert.
+        wait(1);
         
-        int i;
-        for(i=0;i<12;i++)
-        {
-            i2c.write(0x70,cmd1,6); // envoie sur le SAA Nouvelle Génération et sur l'ancien : Car Switch entre les 2. 
-            wait(0.5);
-            i2c.write(0x70,cmd2,6);
-            wait(0.5);
-            i2c.write(0x70,cmd3,6);
-            wait(0.5);
-            i2c.write(0x70,cmd4,6);
-            wait(0.5);
-            i2c.write(0x70,cmd5,6);
-            wait(0.5);
-            i2c.write(0x70,cmd6,6);
-            wait(0.5);  
-            i2c.write(0x70,cmd7,6);
-            wait(0.5);  
-            i2c.write(0x70,cmd8,6);
-            wait(0.5);  
-            i2c.write(0x70,cmd9,6);
-            wait(0.5);  
-            i2c.write(0x70,cmd10,6);
-            wait(0.5);  
-            i2c.write(0x70,cmd11,6);
-            wait(0.5);  
-            i2c.write(0x70,cmd12,6);
-            wait(0.5);        
-        }
-        
+        i2c.write(0x01,cmd2,4); // Eteind la LED.         
         wait(1);
    }
 }
