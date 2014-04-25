60fd164f54b2720296696cafedf2474826671740
Gabi38 <bastiangabriel@hotmail.com>
2014-04-25 13:51:39 +0200

    Compteur 0 à 999 Fonctionnel.
    Envoie sur 2 Afficheur SAA1064.
    Adresse 70 et 72.

--- Mbed_Avec_LCD.c
+++ Mbed_Avec_LCD.c
@@ -1,5 +1,5 @@
 #include "mbed.h"
-#define version "2.8"
+#define version "3.0"
 
 Serial pc(USBTX, USBRX);
 I2C i2c(p28, p27); // SDA, SCL 
@@ -10,44 +10,39 @@ int main()
    wait(1);    
    pc.printf("\n Version = %s \n",version); // Affiche la version du programme sur l'ordinateur (lisible via Putty, HyperTerminal  ou TeraTerm)
    wait(1);
-                           //  1    2    3    4
-   //char cmd[] = {0x00,0x37,0x06,0x5B,0x4F,0x66};
    
-   char cmd[] = {0x00,0x37,0x00,0x00,0x00};
-
+                    // 0      1       2      3      4      5      6       7     8      9
    char chiffre[] = { 0x3F , 0x06 , 0x5B , 0x4F , 0x66 , 0x6D ,  0x7D , 0x07 , 0x7F, 0x6F};
    
 
-   int i = 0;
+   int i = 0, j = 0 , k = 0;
    
    while(1) 
    {
-        if(i> 10 )
+        if(i> 9 )
+        {
             i = 0;
+            j++;
+            if(j>9)
+            {
+                k++;
+                j=0;
+                if(k>9)
+                {
+                    k=0;
+                    j=0;
+                    i=0;
+                }
+            }
+        }
        
+        char cmd[] = {0x00,0x37,0x3F,chiffre[k],chiffre[j],chiffre[i]}; // Commande complete pour faire afficher les chiffres.
         
-        char autre[] = {0x00,0x37,0x00,0x00,0x00,chiffre[i]};
-        pc.printf("\n COMMANDE = %c",autre);
-        
-        i2c.write(0x70,cmd,5);
-        //i2c.write(0x70,autre,1);
-        
-        wait(1);
-        i++;
-        
-        
-/*
-        char cmd2[] = {0x00,0x37,0x00,0x00,0x00,0x5B};
-        i2c.write(0x70,cmd2,6);
-        wait(1.5);
-        
-        char cmd3[] = {0x00,0x37,0x00,0x00,0x00,0x4F};
-        i2c.write(0x70,cmd3,6);
-        wait(1.5);
+        i2c.write(0x72,cmd,6); // envoie sur le SAA1064 ancienne génération.
+        i2c.write(0x70,cmd,6); // envoie sur le SAA Nouvelle Génération
 
-        char cmd4[] = {0x00,0x37,0x00,0x00,0x00,0x66};
-        i2c.write(0x70,cmd4,6);
-        wait(1.5);
-*/
+        wait(1);       
+        i++;
+
    }
 }
